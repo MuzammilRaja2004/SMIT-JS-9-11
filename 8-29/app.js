c3829e0091db55d32c46eac95929b6eb63dc1766
tayyaba-ali <ta6402792@gmail.com>
2025-08-29 10:47:06 +0500

    28 to 33 CH

---
+++ 8-29/app.js
@@ -0,0 +1,39 @@
+var profit = "10" * "2";
+console.log(profit)
+
+var myInteger = parseFloat("1.99999")
+console.log(myInteger)
+
+var integerString = "24"
+var num = Number(integerString);
+
+var numberVal = 1234;
+console.log(numberVal.toString())
+
+var fixedMeth = 10.476645
+console.log(fixedMeth.toFixed(2))
+
+var rightNow = new Date();
+console.log(rightNow.getDay())
+
+
+// var days = [Sun]
+
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var now = new Date();
+var theDay = now.getDay();
+var nameOfToday = dayNames[1];
+console.log(nameOfToday,"nameOfTodaynameOfToday")
+
+
+// var d = new Date();
+// var currentMonth = d.getTime();
+// console.log(currentMonth)
+
+
+
+var doomsday = new Date("Feb 3, 2006");
+console.log(doomsday,"doomsdaydoomsday")
+
+
+
